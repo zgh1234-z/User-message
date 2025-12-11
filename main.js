5dd5bd41e6dbc871e3080565d34d7aa06fa831aa
zgh <ghahremans908@gmil.com>
2025-12-11 12:27:24 +0330

    add the function annd array

---
+++ main.js
@@ -0,0 +1,23 @@
+const Username = Symbol(prompt("نام کاربر "));
+const text = prompt("متن پیام را وارد کنید ");
+
+let M = [];
+
+function message(Name, Text, Time) {
+  this.Name = Name;
+  this.Text = Text;
+  const Time = new Date();
+}
+
+const NewText = new message(Username, text);
+const Newid = new Id(id);
+M.push(NewText);
+M.posh(Newid);
+
+function* Id(n) {
+  let id = 1;
+  while (i <= n) {
+    yield i++;
+  }
+  return id;
+}
